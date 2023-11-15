71a304fac5996bd4cedbf2caba89416dfbcb87dd
= <rhachyd1@gmail.com>
2023-11-15 11:09:54 -0300

    Adding project files and structure

---
+++ src/test/java/com/rhd/spring6simplewebapp/Spring6SimplewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhd.spring6simplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6SimplewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
